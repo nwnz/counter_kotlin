125e0fe68a1ed34080df681a7c2f80a81cfefea3
vvisemetov <v.v.isemetov@gmail.com>
2021-09-04 22:46:24 +0500

    add variable counter

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    var counter: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
